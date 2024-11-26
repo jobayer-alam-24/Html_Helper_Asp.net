24c259e9efb0841b8878c8f684d67ad71c748148
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-11-26 16:21:56 +0600

    html helpers

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Html_Helper_Asp.net.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
